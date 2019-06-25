a4ddccefd8bf1b50536dcc9fdb9dcd866673b1a9
Kuro <body.identity@outlook.com>
2019-06-25 20:36:29 +0200

    uploaded Grapher

---
+++ StatsGraph/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace StatsGraph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown7 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown8 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(71, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(900, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(1046, 636);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(331, 33);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1046, 69);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(331, 561);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(764, 31);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(842, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 51);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(71, 689);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 29);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Lines";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(196, 686);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(234, 686);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(360, 686);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(486, 686);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown3.TabIndex = 10;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(931, 685);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown4.TabIndex = 15;
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.numericUpDown4_ValueChanged);
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(805, 685);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown5.TabIndex = 14;
+            this.numericUpDown5.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown5.ValueChanged += new System.EventHandler(this.numericUpDown5_ValueChanged);
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(679, 685);
+            this.numericUpDown6.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown6.TabIndex = 13;
+            this.numericUpDown6.ValueChanged += new System.EventHandler(this.numericUpDown6_ValueChanged);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(641, 685);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(1046, 22);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(320, 32);
+            this.pictureBox4.TabIndex = 16;
+            this.pictureBox4.TabStop = false;
+            // 
+            // numericUpDown7
+            // 
+            this.numericUpDown7.Location = new System.Drawing.Point(1257, 685);
+            this.numericUpDown7.Maximum = new decimal(new int[] {
+            1200,
+            0,
+            0,
+            0});
+            this.numericUpDown7.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown7.Name = "numericUpDown7";
+            this.numericUpDown7.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown7.TabIndex = 17;
+            this.numericUpDown7.Value = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown8
+            // 
+            this.numericUpDown8.Location = new System.Drawing.Point(1131, 685);
+            this.numericUpDown8.Maximum = new decimal(new int[] {
+            1600,
+            0,
+            0,
+            0});
+            this.numericUpDown8.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown8.Name = "numericUpDown8";
+            this.numericUpDown8.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown8.TabIndex = 18;
+            this.numericUpDown8.Value = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1438, 742);
+            this.Controls.Add(this.numericUpDown8);
+            this.Controls.Add(this.numericUpDown7);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.NumericUpDown numericUpDown7;
+        private System.Windows.Forms.NumericUpDown numericUpDown8;
+    }
+}
+
---
+++ StatsGraph/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace StatsGraph
+{
+    public partial class Form1 : Form
+    {
+        Bitmap pic = new Bitmap(900,600);
+        private int height = 250*2;
+        private int width = 400*2;
+        List<int> v = new List<int>(); 
+        Regex rgx = new Regex(@"^(\d*)$", RegexOptions.Compiled);
+        Stopwatch sw = new Stopwatch();
+        string fileToMonitor;
+        private double xRatio;
+        private double yRatio;
+        private int Color1R = 0;
+        private int Color1G = 0;
+        private int Color1B = 0;
+        private int Color2R = 0;
+        private int Color2G = 100;
+        private int Color2B = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void point(Point p, Color c)
+        {
+            if (p.X > 0 && p.X < pic.Width && p.Y > 0 && p.Y < pic.Height)
+            {
+                pic.SetPixel(p.X, p.Y, c);
+            }
+        }
+
+        private List<Point> line(Point p1, Point p2, Color c)
+        {
+            List<Point> pts = new List<Point>();
+            int dx = Math.Abs(p1.X - p2.X);
+            int dy = Math.Abs(p1.Y - p2.Y);
+            int length = (int)Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2));
+            double m = ((double)p2.Y - p1.Y) / ((double)p2.X - p1.X);
+            double b = p1.Y - m * p1.X;
+            for (int x = (p1.X > p2.X ? p2.X : p1.X); x < dx + (p1.X > p2.X ? p2.X : p1.X); x += 1)
+            {
+                int y = (int)(m * x + b);
+                Point cur = new Point(x, y);
+                pts.Add(cur);
+                point(cur, c);
+            }
+            return pts;
+        }
+
+        private void shadedLine(Point p1, Point p2, Color c)
+        {
+            List<Point> pts = line(p1, p2, c);
+            foreach (var pt in pts)
+            {
+                int curX = pt.X;
+                for (int i = 0; i < pt.Y; i++)
+                {
+                    point(new Point(curX,i), c);   
+                }
+            }
+        }
+
+        private void updataData()
+        {
+            //FileStream fileStream = new FileStream(
+            //    @"c:\words.txt", FileMode.OpenOrCreate,
+            //    FileAccess.ReadWrite, FileShare.None);
+            v = new List<int>();
+            using ( StreamReader sr = new StreamReader(fileToMonitor))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Match m = rgx.Match(line);
+                    v.Add(Int32.Parse(m.Groups[1].Value));
+                }
+                sr.Close();
+                drawData();
+            }
+        }
+
+        private void drawData()
+        {
+            richTextBox1.Text = "";
+            int max = 0;
+            for (var i = 0; i < v.Count; i++)
+            {
+                richTextBox1.Text += $"\nel[{i}]: {v[i]}";
+                if (v[i] > max)
+                {
+                    max = v[i];
+                }
+            }
+
+            // do actuall drawing
+
+            int elements = v.Count;
+            double wiprt = (int)numericUpDown8.Value / (double)elements;
+            double hiprt = (int)numericUpDown7.Value / (double)max;
+            List<Point> points = new List<Point>();
+            double avg = 0;
+            for (var i = 0; i < v.Count; i++)
+            {
+                double xp = (wiprt * i + 30);
+                double yp = (hiprt*v[i]+30);
+                Point p = new Point((int)xp, (int)yp);
+                points.Add(p);
+                avg += v[i];
+            }
+            avg = avg / (double)v.Count;
+            this.richTextBox1.Text += "\n======\n"+avg;
+            pic = new Bitmap(900, 600);
+            int j = 0;
+            // linker ==========
+            Color aColor = Color.FromArgb(Color1R, Color1G, Color1B);
+            Color bColor = Color.FromArgb(Color2R, Color2G, Color2B);
+            
+            List<Point> numCenters = new List<Point>();
+            foreach (var p in points)
+            {
+                //point(p, Color.Black);
+                numCenters.Add(new Point((int)(p.X+wiprt/2),p.Y));
+                if (j % 2 == 1)
+                {
+                    if (!checkBox1.Checked)
+                    {
+                        drawBar(p, (int)wiprt, p.Y, aColor);
+                    }
+                }
+                else
+                {
+                    if (!checkBox1.Checked)
+                    {
+                        drawBar(p, (int)wiprt, p.Y, bColor);
+                    }
+                }
+                j++;
+            }
+            if (checkBox1.Checked)
+            {
+                for (var i = 0; i < numCenters.Count-1; i++)
+                {
+                    Point cur = numCenters[i];
+                    Point next = numCenters[i + 1];
+                    shadedLine(cur,next,i%2==0? aColor : bColor);
+                }
+            }
+            line(new Point(0, (int) avg+50), new Point(500, (int) avg+50), Color.Black);
+            
+            setImage();
+        }
+
+        private void drawBar(Point st, int width, int height, Color c)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    point(new Point(st.X + i, st.Y - j), c);
+                }
+            }
+        }
+
+        private void setImage()
+        {
+            pictureBox1.Image = pic;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                System.Threading.Thread.Sleep(500);
+                worker.ReportProgress(i*10);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (FileToMonitor != "")
+            {
+                updataData();
+            }
+            backgroundWorker1.RunWorkerAsync();
+        }
+        
+        public string FileToMonitor
+        {
+            get => fileToMonitor;
+            set => fileToMonitor = value;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileToMonitor = @"";
+            backgroundWorker1.WorkerReportsProgress = true;
+            sw.Start();
+            backgroundWorker1.RunWorkerAsync();
+            updateColor1();
+            updateColor2();
+            updatePreview();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileToMonitor = textBox1.Text;
+        }
+
+        private void updateColor1()
+        {
+            Bitmap bp = new Bitmap(32,32);
+            for (int i = 0; i < 32; i++)
+            {
+                for (int i1 = 0; i1 < 32; i1++)
+                {
+                    bp.SetPixel(i,i1,Color.FromArgb(Color1R, Color1G, Color1B));
+                }
+            }
+            pictureBox2.Image = bp;
+            updatePreview();
+        }
+
+        private void updateColor2()
+        {
+            Bitmap bp = new Bitmap(32, 32);
+            for (int i = 0; i < 32; i++)
+            {
+                for (int i1 = 0; i1 < 32; i1++)
+                {
+                    bp.SetPixel(i, i1, Color.FromArgb(Color2R, Color2G, Color2B));
+                }
+            }
+            pictureBox3.Image = bp;
+            updatePreview();
+        }
+
+        private void updatePreview()
+        {
+            Bitmap bp = new Bitmap(320, 32);
+            for (int i = 0; i < 320; i++)
+            {
+                for (int i1 = 0; i1 < 32; i1++)
+                {
+                    if ((i / 10) % 2 == 0)
+                    {
+                        bp.SetPixel(i, i1, Color.FromArgb(Color1R, Color1G, Color1B));
+                    }
+                    else
+                    {
+                        bp.SetPixel(i, i1, Color.FromArgb(Color2R, Color2G, Color2B));
+                    }
+                }
+            }
+            pictureBox4.Image = bp;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            Color1R = (int) numericUpDown1.Value;
+            updateColor1();
+        }
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            Color1G = (int) numericUpDown2.Value;
+            updateColor1();
+        }
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            Color1B = (int) numericUpDown3.Value;
+            updateColor1();
+        }
+        private void numericUpDown6_ValueChanged(object sender, EventArgs e)
+        {
+            Color2R = (int) numericUpDown6.Value;
+            updateColor2();
+        }
+        private void numericUpDown5_ValueChanged(object sender, EventArgs e)
+        {
+            Color2G = (int) numericUpDown5.Value;
+            updateColor2();
+        }
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            Color2B = (int) numericUpDown4.Value;
+            updateColor2();
+        }
+    }
+}
